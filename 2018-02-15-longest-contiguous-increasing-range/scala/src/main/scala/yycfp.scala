1a5f5bc23d5a5329dc6bf4e6bcdf9675b18300a2
Andy Czerwonka <andy.czerwonka@gmail.com>
2018-02-16 15:44:34 -0700

    update to use the simpler map/filter

--- 2018-02-15-longest-contiguous-increasing-range/scala/src/main/scala/yycfp.scala
+++ 2018-02-15-longest-contiguous-increasing-range/scala/src/main/scala/yycfp.scala
@@ -4,21 +4,16 @@ package object yycfp {
 
   def longestContiguousIncreasingRange[T: Ordering](xs: Seq[T]): Option[(Int, Int)] = {
     if (xs.nonEmpty) {
-      // we'll use this locally scoped partial to return the index when the current value is <=previous
-      val decreasingChange: PartialFunction[(T, Int), Int] = {
-        case (x, i) if i > 0 && implicitly[Ordering[T]].lteq(x, xs(i - 1)) => i
-      }
+      val indices = for {
+        i <- xs.indices
+        if i > 0 && implicitly[Ordering[T]].lteq(xs(i), xs(i-1))
+      } yield i
 
-      // detect all the changes based on the decreasingChange function above
-      val changes = 0 +: xs.zipWithIndex.collect(decreasingChange) :+ xs.size
-
-      // find the longest chain
-      val (index, length) = changes
+      val (index, length) = (0 +: indices :+ xs.size)
         .sliding(2)
         .map({ case Seq(l, r) => (l, r - l) })
         .maxBy(_._2)
-
-      // calculate the two indexes
+      
       Some(index, index + length - 1)
     } else None
   }
