bf1aad1e2712542f22a4b51b87dc64487d91e11f
Vinicius Gravina da Rocha <vinicius.darocha@gmail.com>
2018-09-15 12:16:25 -0600

    Add comments to example3

--- functors/example3/example3.ml
+++ functors/example3/example3.ml
@@ -1,32 +1,34 @@
 open Base
 
-(* Implementing Haskell's Data.Sort
-   sort: (Ord a) => [a] -> [a] *)                         
+(* Let's implement Haskell's Data.Sort
 
-module type Sortable = sig
+     sort: (Ord a) => [a] -> [a]
+*)
+
+(* Let's define what it means to be a module of type `Ord`: *)
+module type Ord = sig
   type t
   val compare: t -> t -> int
 end
 
+(* Given a module `X` that has the `Ord` signature, the functor will produce a
+   `sort` functions for that module *)
 module SortableList
-         (X: Sortable) = struct
+         (X: Ord) = struct
   let sort ls = List.sort ~compare:X.compare ls
 end
 
+(* Instantiating a list of integers that is sortable *)
 module IntSortableList =
   SortableList(struct
       type t=int
       let compare = Int.compare
     end)
 
+(* And a list of strings that is sortable *)
 module StringSortableList =
   SortableList(struct
       type t=string
       let compare = String.compare
     end)
 
-
-
-(* In OCaml, you have to be explicit about everything
-   - downside: can't do generic programming
-   - upside: you are explicit about it and always know what's being used in your program *)
