ae2343a8f81cfdc36513ce83b21a521e44079744
Vinicius Gravina da Rocha <vinicius.darocha@gmail.com>
2018-09-15 12:15:59 -0600

    Add comments to example2

--- functors/example2/example2.mli
+++ functors/example2/example2.mli
@@ -5,6 +5,8 @@ module Queue : sig
           
   val enqueue: 'a t -> 'a -> 'a t
   val dequeue: 'a t -> ('a * 'a t) option
+
+  (* Including the Container signature to expose Container-specific functions *)
                                    
   include Base.Container.S1 with type 'a t := 'a t
 end
